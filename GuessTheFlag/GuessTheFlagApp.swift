6872fad93b1164cef7245911a89400fb3e8fdd61
Filipe Fernandes <filipeferibeiro@gmail.com>
2025-06-11 18:12:07 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Filipe Fernandes on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
